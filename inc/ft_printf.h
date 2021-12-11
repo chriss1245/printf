34c03da926c09fd36fcbfbf605c95eb61802b4b4
Christopher Manzano Vimos <cmanzano@student.42madrid.com>
2021-12-11 18:54:27 +0100

    added needed functions to ft_printf.h

--- inc/ft_printf.h
+++ inc/ft_printf.h
@@ -6,7 +6,7 @@
 /*   By: cmanzano <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/08 11:50:14 by cmanzano          #+#    #+#             */
-/*   Updated: 2021/12/08 14:12:58 by cmanzano         ###   ########.fr       */
+/*   Updated: 2021/12/11 18:30:00 by cmanzano         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,11 @@
 # include "../libft/libft.h"
 # include <stdarg.h>
 
-int	ft_printf(const char *s, ...);
-
+int	ft_printf(const char *s, ...) __attribute__((format(printf, 1, 2)));
+int	put_number(int n);
+int	put_unsigned_number(unsigned int n);
+int	put_string(char *str);
+int put_char(int c);
+int	put_hex(unsigned int n, char a);
+int	put_ptr(void *p);
 #endif
